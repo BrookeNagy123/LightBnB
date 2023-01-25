f093eb8fb172f0ca09619f84bbd8f4c172ea08e2
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-01-24 18:40:35 -0700

    Update to syntax

--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -2,4 +2,4 @@ SELECT properties.city, COUNT(reservations.id) as total_reservations
 FROM properties 
 JOIN reservations ON properties.id = property_id
 GROUP BY properties.city
-ORDER BY total_reservations DESC
+ORDER BY total_reservations DESC;
